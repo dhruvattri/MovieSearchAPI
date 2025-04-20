79d8aaed42c2223acde9d43bc89ac35295bed39e
Dhruvattri <dhruvattri2@gmail.com>
2025-04-20 16:16:50 -0400

    Set up basic response check and empty result div

--- JS/script.js
+++ JS/script.js
@@ -3,10 +3,18 @@ const apiKey = 'dcc707b6';
 function searchMovies() {
     const query = document.getElementById('searchInput').value;
     const url = `https://www.omdbapi.com/?s=${query}&apikey=${apiKey}`;
-    
+
     fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data); 
-    });
+        .then(res => res.json())
+        .then(data => {
+            const resDiv = document.getElementById('results');
+            resDiv.innerHTML = ''; // clear previous results
+
+            if (data.Response === 'True') {
+                // TODO: loop through movies and display posters
+
+            } else {
+                resDiv.innerHTML = 'No movies found.';
+            }
+        });
 }
