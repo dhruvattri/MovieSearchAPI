78d8e229be0372f871fc9558819f63555d0935f1
Dhruvattri <dhruvattri2@gmail.com>
2025-04-21 00:17:42 -0400

    improved trailer box layout

--- JS/movie.js
+++ JS/movie.js
@@ -19,17 +19,25 @@ function displayMovieDetails(movie) {
 
     detailsDiv.innerHTML = `
         <h2 class="box">${movie.Title} (${movie.Year})</h2>
+        
         <div class="box">
             <img src="${movie.Poster !== 'N/A' ? movie.Poster : 'placeholder.jpg'}" alt="${movie.Title}">
         </div>
+        
         <div class="box"><p><b>Genre:</b> ${movie.Genre}</p></div>
         <div class="box"><p><b>Plot:</b> ${movie.Plot}</p></div>
         <div class="box"><p><b>Director:</b> ${movie.Director}</p></div>
         <div class="box"><p><b>Actors:</b> ${movie.Actors}</p></div>
         <div class="box"><p><b>IMDB Rating:</b> ${movie.imdbRating}</p></div>
-        <div id="trailerContainer"><h3>Loading trailer...</h3></div>
+        
+        <div id="trailerContainer">
+            <div class="trailer-box">
+                <h3 style="text-align: center;">Loading trailer...</h3>
+            </div>
+        </div>
     `;
 }
+
 function fetchYouTubeTrailer(title) {
     const searchQuery = encodeURIComponent(`${title} trailer`);
     const youtubeURL = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${searchQuery}&key=${youtubeApiKey}&type=video`;
